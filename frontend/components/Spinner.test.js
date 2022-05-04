1904bfe1585820f6bdf6b2fe6e22a83dcefeb826
Lucas Bigelow <lukejbigelow@gmail.com>
2022-05-04 12:54:51 -0500

    did the required MVP testing of the spinner component

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,20 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Spinner from './Spinner';
+
+describe('Spinner Tests', () => {
+  test('Spinner renders when passed prop is true', () => {
+    const result = render(<Spinner on={true} />);
+    const spinner = result.container.querySelector('#spinner');
+    expect(spinner).toBeInTheDocument;
+  })
+  test('Spinner will not render when passed prop is false', () => {
+    const result = render(<Spinner on={false} />)
+    const spinner = result.container.querySelector('#spinner');
+    expect(spinner).toBeUndefined;
+  })
 })
